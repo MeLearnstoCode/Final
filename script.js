1f7f78d3b3fd3d7110ba5732f354f48cd479b65e
Nabila Fahreen <nfahreen@gmail.com>
2017-05-08 20:01:47 -0400

    updated js to include background images for containers

--- script.js
+++ script.js
@@ -14,7 +14,40 @@ $(document).ready(function(){
 	});
 
 
-$(document).ready(function(){
-
+$(function(){
+   $('#how').on('focus mouseenter', function(){
+    $(this).parent().css("background", "url(closet5.jpg) no-repeat");
+   });
 });
 
+$(function(){
+   $('#how2').on('focus mouseenter', function(){
+    $(this).parent().css("background", "url(closet1.jpg) no-repeat");
+   });
+});
+	
+$(function(){
+   $('#how3').on('focus mouseenter', function(){
+    $(this).parent().css("background", "url(closet2.jpg) no-repeat");
+   });
+});
+	
+$(function(){
+   $('#how4').on('focus mouseenter', function(){
+    $(this).parent().css("background", "url(closet3.jpg) no-repeat");
+   });
+});
+
+$(function(){
+   $('#how5').on('focus mouseenter', function(){
+    $(this).parent().css("background", "url(closet4.jpg) no-repeat");
+   });
+});
+
+$(function(){
+   $('.row').on('focus mouseenter', function(){
+    $("h3").css("backgroundColor", "rgba(128,128,128,.5)");
+   });
+});
+
+	
